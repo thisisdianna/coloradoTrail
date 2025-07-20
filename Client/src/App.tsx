9b8a4d0fd049795a41ac68e8641473492af7c577
Dianna Gomez <diannamelendezgomez@gmail.com>
2025-07-20 16:12:51 -0700

    absolute most basic front page

---
+++ Client/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const trailName = "Colorado Trail";
+  
+  return (
+    <div>
+      <h1>Hello, {trailName}!</h1>
+    </div>
+  );
+}
+
+export default App;
